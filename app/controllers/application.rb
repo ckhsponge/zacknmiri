06b2c9a32637fe6873bcb68226f3dc7f67b95450
Chris Hobbs <chris@spongecell.com>
2008-11-18 17:03:04 -0800

    redirect to base

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  HOST = "zacknmiri.com"
   helper :all # include all helpers, all the time
+  
+  before_filter :redirect_to_base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -12,4 +15,17 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  protected
+  def redirect_to_base
+    unless development_mode? || HOST==request.host
+      redirect_to params.update(:host=>HOST)
+      return false #stop filter chain
+    end
+  end
+  
+  def development_mode?
+    RAILS_ENV != 'production'
+  end
+  
 end
